beefb4fd475b99bdedd355a7a9e0a1b72557ce19
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-03-03 14:16:07 +0000

    implementacion del sexto codigo

--- Apuntadores--03.03.2023/codigo_6.c
+++ Apuntadores--03.03.2023/codigo_6.c
@@ -7,8 +7,9 @@ int main(){
     short nums[] = {55,44,33,22,11};
     short *ap, cont;
     ap = nums;
+
     for (cont = 0; cont < 5 ; cont++)
-    printf("%x\n",(ap+cont));
+        printf("%x\n",(ap+cont));
 
     return 0;
 }
