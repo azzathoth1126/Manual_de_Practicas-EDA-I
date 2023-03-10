22b7fe916f0d4c8db31bdc201c8915a76cb9d1f8
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-03-10 13:34:12 +0000

    codigo 1 listo

--- p3_estructuras_10.03.23/codigo_01.c
+++ p3_estructuras_10.03.23/codigo_01.c
@@ -10,31 +10,22 @@ struct pelicula{
 };
 
 void imprimirDatosPelicula(struct pelicula);
-struct nodo llenarDatosPelicula(char *, char *, short , short , char*[10]);
+
+struct pelicula llenarDatosPelicula(char *, char *, short , short , char*[10]);
+
 
 int main(){
+
     char *directores[10];
 
     directores[0] = "Lana Wachowski";
     directores[1] = "Andy Wachowski";
+
     struct pelicula matrix = llenarDatosPelicula("The matrix", "Cienciaficción", 1999, 2, directores);
+
     imprimirDatosPelicula(matrix);
     
     return 0;
-}
-
-struct nodo llenarDatosPelicula(char *nombre, char *genero, short anio, short numDirectores, char *directores[10]){
-    struct pelicula movie;
-    movie.nombre = nombre;
-    movie.genero = genero;
-    movie.anio = anio;
-    movie.numDirectores = numDirectores;
-    
-    int cont = 0;
-    for ( ; cont < movie.numDirectores ; cont++)
-        movie.directores[cont] = directores[cont];
-    
-    return movie;
 }
 
 void imprimirDatosPelicula(struct pelicula movie){
@@ -43,8 +34,21 @@ void imprimirDatosPelicula(struct pelicula movie){
     printf("ANIO: %d\n", movie.anio);
     printf("DIRECTOR(ES):\n");
 
-    int cont = 0;
-    for ( ; cont < movie.numDirectores ; cont++)
+    
+    for (int cont = 0; cont < movie.numDirectores ; cont++)
         printf("%s\n", movie.directores[cont]);
 
 }
+
+struct pelicula llenarDatosPelicula(char *nombre, char *genero, short anio, short numDirectores, char *directores[10]){
+    struct pelicula movie;
+    movie.nombre = nombre;
+    movie.genero = genero;
+    movie.anio = anio;
+    movie.numDirectores = numDirectores;
+    
+    for (int cont = 0; cont < movie.numDirectores ; cont++)
+        movie.directores[cont] = directores[cont];
+    
+    return movie;
+};
