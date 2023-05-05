7d2e1fbb79f5f640e7045fc34280429ba3b3e466
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-05-05 14:37:48 +0000

    Codigo de la octava sema del laboratorio

--- p8_python_05.05.23/p1.py
+++ p8_python_05.05.23/p1.py
@@ -1,1 +1,105 @@
-print("Hola")
+'''
+Notas de clase o recordatorios 05.05.2023
+'''
+
+
+
+#INTRODUCCION
+
+'''
+Cometario tipos de variables
+#El interprete ya sabe que tipo de variable es automaticamente
+
+entera = 3  int
+flotante = 3.1415   float
+cadena = "hola"  str
+booleano = True  bool
+'''
+
+#Las funciones pueden resivir como parametros otras funciones con sus respectivos parametros
+
+''' 
+Funciones para imprimir en pantalla y leer datos 
+
+print()
+imput()
+
+print puede concatenar cadenas pero ambas tienen que tener formato de str
+'''
+
+#Math
+
+'''
+ceil - baja el numero a su siguiente mas cercano: ceil(3.4) = 3
+floor - sube el numero a su siguiente mas cercano: floor(3.6) = 4
+'''
+
+#Archivos
+
+'''
+Los archivos son mejor dejarlo en la misma carpeta de lo contrario tendras que
+poner toda la ruta de su ubicacion
+
+Es necesario usar la funcion close para que no mande error en interprete
+'''
+
+
+
+#CADENAS
+
+'''
+Las cadenas son inmutables 
+Las cadenas pueden tener otras cadenas dentro de de las mismas
+
+comillas simple - acepta solo 1 palabra
+comillas doble - acepta palabras con espacios
+comillas triple - acepta palabras con espacion y saltos de lineas
+'''
+
+#Title
+
+'''
+Toma una cade y cada que localiza un espacion transforma el primer caracter y la convierte en 
+mayuscula
+'''
+
+#Split
+
+'''
+split transaforma una cade en un arreglo
+array.split() - transforma cada elemento de la cadena en elemento de un arreglo
+array.split(\n) - transdorma en elementos cada que encuentra un salto de linea
+'''
+
+#isnumeric, isdecimal
+
+'''
+Buscan si hay numeros y devuelve True o False dependeido de si la busqueda es exitosa
+incluso dentro de las cadenas
+'''
+
+#SubCadenas
+
+'''
+cadena[4] - imprime solo el elemento cuatro de da la cadena
+cadena[-2] - al comportarde como 
+cadena[4:] -  imprime toda la cadena a partir del quinto elemeto
+cadena[:4] - imprime desde el primer elemento hasta el quinto elemeto
+'''
+
+
+
+#Practica 9
+
+''' 
+Buscar como crear un cuaderno ipynb en python 
+
+y hacer todos los ejemplos de la practica en el cuadermo
+'''
+
+
+'''
+Para imprimir en python se usa la siguiente sintaxis:
+python3 nom_archivo.py
+python3 nom_archivo.ipynb e imprime todos los ejemplos del cuaderno
+'''
